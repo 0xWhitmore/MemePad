442e77904ad3e2f676e2648b7c06ac55ceb22e4f
0xWhitmore <oo0n6drxp@hotmail.com>
2025-02-11 15:47:23 +0800

    init project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MemePad</h1>
+        <p>Create, mint and trade meme NFTs</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
